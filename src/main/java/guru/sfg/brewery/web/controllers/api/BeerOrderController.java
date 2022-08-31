20b9eb6145e58216e72a3094a30fdab1d35672f6
Ilona <ilonascientist@gmail.com>
2022-08-31 10:47:57 +0300

    Multi-tenancy: add custom annotations

--- src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/api/BeerOrderController.java
@@ -1,5 +1,7 @@
 package guru.sfg.brewery.web.controllers.api;
 
+import guru.sfg.brewery.security.perms.BeerOrderCreatePermission;
+import guru.sfg.brewery.security.perms.BeerOrderReadPermission;
 import guru.sfg.brewery.services.BeerOrderService;
 import guru.sfg.brewery.web.model.BeerOrderDto;
 import guru.sfg.brewery.web.model.BeerOrderPagedList;
@@ -25,43 +27,37 @@ public class BeerOrderController {
         this.beerOrderService = beerOrderService;
     }
 
-    @PreAuthorize("hasAuthority('order.read') " +
-            "OR hasAuthority('customer.order.read') " +
-            "AND @beerOrderAuthenticationManager.customerIdMatchers(authentication, #customerId)")
+    @BeerOrderReadPermission
     @GetMapping("orders")
     public BeerOrderPagedList listOrders(@PathVariable("customerId") UUID customerId,
-                                         @RequestParam(value = "pageNumber",required = false) Integer pageNumber,
-                                         @RequestParam(value = "pageSize", required = false) Integer pageSize){
+                                         @RequestParam(value = "pageNumber", required = false) Integer pageNumber,
+                                         @RequestParam(value = "pageSize", required = false) Integer pageSize) {
 
-        if(pageNumber == null || pageNumber < 0) pageNumber = DEFAULT_PAGE_NUMBER;
-        if(pageSize == null || pageSize < 1) pageSize = DEFAULT_PAGE_SIZE;
+        if (pageNumber == null || pageNumber < 0) pageNumber = DEFAULT_PAGE_NUMBER;
+        if (pageSize == null || pageSize < 1) pageSize = DEFAULT_PAGE_SIZE;
 
         return beerOrderService.listOrders(customerId, PageRequest.of(pageNumber, pageSize));
 
     }
 
-    @PreAuthorize("hasAuthority('order.create') " +
-            "OR hasAuthority('customer.order.create') " +
-            "AND @beerOrderAuthenticationManager.customerIdMatchers(authentication, #customerId)")
+    @BeerOrderCreatePermission
     @PostMapping("orders")
     @ResponseStatus(HttpStatus.CREATED)
     public BeerOrderDto placeOrder(@PathVariable("customerId") UUID customerId,
-                                   @RequestBody BeerOrderDto beerOrderDto){
+                                   @RequestBody BeerOrderDto beerOrderDto) {
         return beerOrderService.placeOrder(customerId, beerOrderDto);
     }
 
-    @PreAuthorize("hasAuthority('order.read') " +
-            "OR hasAuthority('customer.order.read') " +
-            "AND @beerOrderAuthenticationManager.customerIdMatchers(authentication, #customerId)")
+    @BeerOrderReadPermission
     @GetMapping("orders/{orderId}")
     public BeerOrderDto getOrder(@PathVariable("customerId") UUID customerId,
-                                 @PathVariable("orderId") UUID orderId){
+                                 @PathVariable("orderId") UUID orderId) {
         return beerOrderService.getOrderById(customerId, orderId);
     }
 
     @PutMapping("/orders/{orderId}/pickup")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void pickupOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId){
+    public void pickupOrder(@PathVariable("customerId") UUID customerId, @PathVariable("orderId") UUID orderId) {
         beerOrderService.pickupOrder(customerId, orderId);
     }
 
