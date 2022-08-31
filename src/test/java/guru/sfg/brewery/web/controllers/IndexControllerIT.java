1ded6db82de414bc759dbf23862864656eba057a
Ilona <ilonascientist@gmail.com>
2022-08-31 07:40:15 +0300

    Multi-tenancy: BeerOrderController

--- src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/IndexControllerIT.java
@@ -3,11 +3,11 @@ package guru.sfg.brewery.web.controllers;
 import guru.sfg.brewery.repositories.BeerInventoryRepository;
 import guru.sfg.brewery.repositories.BeerRepository;
 import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.services.BeerOrderService;
 import guru.sfg.brewery.services.BeerService;
 import guru.sfg.brewery.services.BreweryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -30,6 +30,9 @@ class IndexControllerIT extends BaseIT{
 
     @MockBean
     public BreweryService breweryService;
+
+    @MockBean
+    BeerOrderService beerOrderService;
 
     @Test
     void testGetIndexSlash() throws Exception {
